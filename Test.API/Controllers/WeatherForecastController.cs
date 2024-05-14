95d571bc55ab00666c6ea4851fb7c6a3a49a10de
Majd <majd-louka@hotmail.com>
2024-05-14 20:02:15 +0300

    try the workflow

--- Test.API/Controllers/WeatherForecastController.cs
+++ Test.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,8 @@ namespace Test.API.Controllers
 	{
 		private static readonly string[] Summaries = new[]
 		{
-			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching",
+			"Freezing as fuck", "Testing my first workflow", "I love you all"
 		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
@@ -21,7 +22,7 @@ namespace Test.API.Controllers
 		[HttpGet(Name = "GetWeatherForecast")]
 		public IEnumerable<WeatherForecast> Get()
 		{
-			return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+			return Enumerable.Range(1, 11).Select(index => new WeatherForecast
 			{
 				Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
 				TemperatureC = Random.Shared.Next(-20, 55),
@@ -31,4 +32,4 @@ namespace Test.API.Controllers
 		}
 	}
 }
-//now ready to test
+//now ready to integrate
